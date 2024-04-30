875a63741e23cd91ff16bc8a4b9160c396eabdb0
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-01 07:29:18 +1000

    Add handleAddNewProject function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
+import { useRef, useState } from 'react';
+import NoProjectSelected from './components/NoProjectSelected';
+import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
+
 export default function App() {
-	return <div>Hello World!</div>;
+	const [projectData, setProjectData] = useState({
+		selectedProjectId: undefined,
+	});
+
+	function handleAddNewProject() {
+		setProjectData(prevProjectData => {
+			return {
+				...prevProjectData,
+				selectedProjectId: null,
+			};
+		});
+	}
+
+	const newProjectInputRef = useRef(null);
+
+	function handleNewProjectSubmit() {
+		alert(newProjectInputRef.current.date.value);
+	}
+
+	let content = <NoProjectSelected onAddProject={handleAddNewProject} />;
+
+	if (projectData.selectedProjectId === null) {
+		content = (
+			<NewProject
+				ref={newProjectInputRef}
+				onProjectSave={handleNewProjectSubmit}
+			/>
+		);
+	}
+
+	return (
+		<main className="h-screen my-8 flex gap-8">
+			<ProjectsSidebar onAddProject={handleAddNewProject} />
+			{content}
+		</main>
+	);
 }
 
