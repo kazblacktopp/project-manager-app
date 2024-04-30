75752beca0e5bd984a5b19c912708f5911946c1d
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-01 09:14:26 +1000

    Add handleNewProjectSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,36 @@ import NewProject from './components/NewProject';
 export default function App() {
 	const [projectData, setProjectData] = useState({
 		selectedProjectId: undefined,
+		projects: [],
 	});
+	const [valueIsValid, setValueIsValid] = useState(true);
+
+	const newProjectInputRef = useRef(null);
+
+	function handleNewProjectSubmit() {
+		const title = newProjectInputRef.current.title.value;
+		const description = newProjectInputRef.current.description.value;
+		const date = newProjectInputRef.current.date.value;
+
+		if (!validateValueIsValid([title, description, date])) {
+			setValueIsValid(false);
+			return;
+		}
+
+		const newProject = {
+			title,
+			description,
+			date,
+		};
+
+		setProjectData(prevProjectData => {
+			return {
+				...prevProjectData,
+				selectedProjectId: newProject.title,
+				projects: [...prevProjectData.projects, newProject],
+			};
+		});
+	}
 
 	function handleAddNewProject() {
 		setProjectData(prevProjectData => {
@@ -24,15 +53,22 @@ export default function App() {
 				selectedProjectId: undefined,
 			};
 		});
+		setValueIsValid(true);
 	}
 
-	const newProjectInputRef = useRef(null);
+	function validateValueIsValid(values) {
+		let isValid = true;
 
-	function handleNewProjectSubmit() {
-		alert(newProjectInputRef.current.date.value);
+		for (const value of values) {
+			if (value.length < 1) {
+				isValid = false;
+				break;
+			}
+		}
+		return isValid;
 	}
 
-	let content = <NoProjectSelected onAddProject={handleAddNewProject} />;
+	let content = <div>{projectData.selectedProjectId}</div>;
 
 	if (projectData.selectedProjectId === null) {
 		content = (
@@ -44,10 +80,15 @@ export default function App() {
 		);
 	}
 
+	if (projectData.selectedProjectId === undefined) {
+		content = <NoProjectSelected onAddProject={handleAddNewProject} />;
+	}
+
 	return (
 		<main className="h-screen my-8 flex gap-8">
 			<ProjectsSidebar onAddProject={handleAddNewProject} />
 			{content}
+			{!valueIsValid && <p>The input is NOT valid!</p>}
 		</main>
 	);
 }
