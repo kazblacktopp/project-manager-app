f23ae711e943321d811f57c6e29318798d345c48
kazblacktopp <karen.blacktopp@gmail.com>
2024-05-01 12:33:24 +1000

    Add key prop to dynamic list items

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -14,7 +14,7 @@ export default function ProjectsSidebar({
 			<ul className="mt-8">
 				{projects.map(project => {
 					return (
-						<li>
+						<li key={project.title}>
 							<button
 								onClick={() => onSelectProject(project)}
 								className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 text-stone-400"
